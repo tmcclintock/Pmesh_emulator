dabed43310734f50d3be0033738ee96dd1340c15
Tom McClintock <tmcclintock89@gmail.com>
2019-06-04 13:31:24 -0400

    Making the software directory.

---
+++ pmesh_emulator/__init__.py
@@ -0,0 +1,9 @@
+"""Emulator for particle mesh sims.
+
+"""
+from .pmeshemulator import pmeshemulator
+assert pmeshemulator #A hack to get pyflakes to not complain
+
+__version__ = "0.1.0"
+__author__ = "Thomas McClintock"
+__email__ = "mcclintock@bnl.gov"
